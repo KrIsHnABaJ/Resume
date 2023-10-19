7c9b2f8126be04629a657caa6d3fc999a75fccbf
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-10-19 20:18:30 +0530

    Add files via upload

---
+++ src/components/Resume.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+export default class Resume extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {resumeData.education &&
+              resumeData.education.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.UniversityName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfPassing} {item.YearOfPassing}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {resumeData.work &&
+              resumeData.work.map((item) => {
+                return (
+                  <div className="row item">
+                    <div className="twelve columns">
+                      <h3>{item.CompanyName}</h3>
+                      <p className="info">
+                        {item.specialization}
+                        <span>&bull;</span>{" "}
+                        <em className="date">
+                          {item.MonthOfLeaving} {item.YearOfLeaving}
+                        </em>
+                      </p>
+                      <p>{item.Achievements}</p>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <p>{resumeData.skillsDescription}</p>
+
+            <div className="bars">
+              <ul className="skills">
+                {resumeData.skills &&
+                  resumeData.skills.map((item, index) => {
+                    // Calculate the width based on the length of the skill name
+                    const skillNameLength = item.skillname.length;
+
+                    // Define a CSS class based on the skill name
+                    const barClassName = `bar-expand ${item.skillname.toLowerCase()}`;
+
+                    // Set a minimum width for the bars
+                    const minWidth = "20px";
+
+                    return (
+                      <li key={index}>
+                        <span className="bar">
+                          <span
+                            className={barClassName}
+                            style={{
+                              width: `calc(${skillNameLength+60}ch + ${minWidth})`,
+                            }}
+                          ></span>
+                        </span>
+                        <em>{item.skillname}</em>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,96 @@
+let resumeData = {
+
+    "name": "Krishna Bajpai",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/krishna-bajpai-10bb56251/",
+    "roleDescription": "I am curretnly learning MERN Full stack and I am brushing up my skills in domains of JAVA and Python.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/krishna-bajpai-10bb56251/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/KrIsHnABaJ",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Vellore Institute of Technology and pursuing my B.Tech in Information Technology. I am currently learning Full Stack Web Development and brushing up my skills Python and Java. I am also aiming to delve into world of Ai and Ml in near future.",
+    "address":"India",
+    "website":"krishnabajapi28@gmail.com",
+    "education":[
+      {
+        "UniversityName":"The Vellore Insitute of Technology",
+        "specialization":"Artificial Intelligence and Machine Learning",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2025"
+        
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Omdenaa",
+        "specialization":"Artificial Intelligence and Machine Learning",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2023",
+        "Achievements":"Finalist in Unstop 'TATA Imagination Challenge'"
+      },
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"JAVA"
+      },
+      {
+        "skillname":"Python"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project1",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+        "url":"https://arnav240703.github.io/html_css_0026/"
+      },
+      {
+        "name":"Project2",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project3",
+        "description":"mobileapp",  
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      },
+      {
+        "name":"Project4",
+        "description":"mobileapp",
+        "imgurl":"https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80"
+      }
+    ],
+    "testimonials":[
+      
+      {
+        "description":"Your AI, ML skills are impressive and consistently deliver outstanding results.",
+        "name":"Meet Movaliya"
+      }
+    ]
+  }
+  
+  export default resumeData
